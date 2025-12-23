bac3c431557bd37e8b47208b17ec3ae7b574dc7b
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2025-12-23 01:17:24 +0100

    Add dummy startup message

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"log"
+
 	serviceConfig "github.com/SnackLog/service-config-lib"
 )
 
 func main() {
 	serviceConfig.LoadConfig()
+	log.Println(serviceConfig.GetConfig().ServiceName)
 }
