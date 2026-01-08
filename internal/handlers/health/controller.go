19cc52bc3e32f5209755efe13aaff01fe41f589e
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2026-01-08 13:56:46 +0100

    Fix doc comment

--- internal/handlers/health/controller.go
+++ internal/handlers/health/controller.go
@@ -2,7 +2,7 @@ package health
 
 import "database/sql"
 
-// SessionController Controller for sessions, to be used by Gin
+// HealthController Controller for sessions, to be used by Gin
 type HealthController struct {
 	DB *sql.DB
 }
