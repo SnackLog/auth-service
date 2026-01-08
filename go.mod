87c6bc1025fe9e6457ebbed06526634ae0aa9347
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2026-01-08 16:46:42 +0100

    Add skeleton handler for user auth and hash checking

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 )
 
 require (
+	github.com/LightJack05/argon-hash-utils v0.0.1
 	github.com/bytedance/sonic v1.14.0 // indirect
 	github.com/bytedance/sonic/loader v0.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
@@ -24,7 +25,7 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/lib/pq v1.10.9
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
@@ -35,12 +36,12 @@ require (
 	github.com/ugorji/go/codec v1.3.0 // indirect
 	go.uber.org/mock v0.5.0 // indirect
 	golang.org/x/arch v0.20.0 // indirect
-	golang.org/x/crypto v0.45.0 // indirect
-	golang.org/x/mod v0.29.0 // indirect
+	golang.org/x/crypto v0.46.0 // indirect
+	golang.org/x/mod v0.30.0 // indirect
 	golang.org/x/net v0.47.0 // indirect
-	golang.org/x/sync v0.18.0 // indirect
-	golang.org/x/sys v0.38.0 // indirect
-	golang.org/x/text v0.31.0 // indirect
-	golang.org/x/tools v0.38.0 // indirect
+	golang.org/x/sync v0.19.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+	golang.org/x/text v0.32.0 // indirect
+	golang.org/x/tools v0.39.0 // indirect
 	google.golang.org/protobuf v1.36.9 // indirect
 )
--- internal/handlers/sessionhandler/post.go
+++ internal/handlers/sessionhandler/post.go
@@ -1,12 +1,54 @@
 package sessionhandler
 
 import (
+	"fmt"
 	"net/http"
 
+	argonhashutils "github.com/LightJack05/argon-hash-utils"
+	"github.com/SnackLog/auth-service/internal/database/user"
 	"github.com/gin-gonic/gin"
 )
 
+type loginRequestBody struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
 // Post Creates a new session
 func (s *SessionController) Post(c *gin.Context) {
-	c.AbortWithStatus(http.StatusNotImplemented)
+	var body loginRequestBody
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+		return
+	}
+
+	message, err := s.authenticateUser(body.Username, body.Password)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Authentication failed"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": message})
+}
+
+func (s *SessionController) authenticateUser(username, password string) (string, error) {
+	user, err := user.GetUserByUsername(s.DB, username)
+	if err != nil {
+		// TODO: Should we leak this to the client?
+		return "", fmt.Errorf("failed to get user: %v", err)
+	}
+	if user == nil {
+		return "", fmt.Errorf("user not found")
+	}
+
+	hash, err := argonhashutils.ParseHash(user.PasswordHash)
+	if err != nil {
+		// TODO: Should we leak this to the client?
+		return "", fmt.Errorf("failed to parse password hash: %v", err)
+	}
+
+	sentHash := argonhashutils.HashPassword(password, hash.Memory, hash.Time, hash.Parallelism, hash.Salt, uint32(len(hash.Hash)))
+	if !argonhashutils.CompareHashes(hash, sentHash) {
+		return "", fmt.Errorf("invalid password")
+	}
+	return "success!", nil
 }
