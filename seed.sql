8467ece3d5c0215a38f760c920904c2d4cdaec87
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2026-01-12 17:12:47 +0100

    Added cred hint to seed data

--- seed.sql
+++ seed.sql
@@ -1,3 +1,3 @@
 TRUNCATE TABLE users;
 INSERT INTO users (username, display_name, password_hash) VALUES
-('foo', 'FOO', '$argon2id$v=19$m=65536,t=2,p=1$VkE0clVVL2ZaYlI1d05nV1U0cW9vdz09$8/0wJxQmno62Q5x7FhgX++zwSLGV8F+ySU9aNTLDj4Q');
+('foo', 'FOO', '$argon2id$v=19$m=65536,t=2,p=1$VkE0clVVL2ZaYlI1d05nV1U0cW9vdz09$8/0wJxQmno62Q5x7FhgX++zwSLGV8F+ySU9aNTLDj4Q'); -- Creds: User:foo PW:foo
