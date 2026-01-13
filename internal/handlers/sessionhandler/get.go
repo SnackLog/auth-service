0d66bed84a02d38be3ae7525ad53362044608bb2
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2026-01-13 14:33:46 +0100

    Added session get endpoint to be used by auth middleware

---
+++ internal/handlers/sessionhandler/get.go
@@ -0,0 +1,37 @@
+package sessionhandler
+
+import (
+	"net/http"
+
+	"github.com/SnackLog/auth-service/internal/crypto"
+	revokedtokens "github.com/SnackLog/auth-service/internal/database/revoked_tokens"
+	"github.com/gin-gonic/gin"
+)
+
+// GetID Retrieves session by ID
+func (s *SessionController) GetID(c *gin.Context) {
+	authHeader := c.GetHeader("Authorization")
+	if authHeader == "" {
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+	claims, err := crypto.ParseAndValidateToken(authHeader)
+	if err != nil {
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	revoked, err := revokedtokens.IsTokenRevoked(s.DB, claims.ID)
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+	if revoked {
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"username": claims.Subject,
+	})
+}
