7914c3f1150d25674c2b9a5688ea3e279f25d9f3
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2026-01-13 15:49:30 +0100

    Added user get endpoint

--- internal/handlers/userhandler/get.go
+++ internal/handlers/userhandler/get.go
@@ -3,10 +3,32 @@ package userhandler
 import (
 	"net/http"
 
+	"github.com/SnackLog/auth-service/internal/database/user"
 	"github.com/gin-gonic/gin"
 )
 
+type userGetResponse struct {
+	Username    string
+	DisplayName string
+}
+
 // Get Retrieves an existing user
 func (u *UserController) Get(c *gin.Context) {
-	c.AbortWithStatus(http.StatusNotImplemented)	
+	username := c.GetString("username")
+	user, err := user.GetUserByUsername(u.DB, username)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve user"})
+		return
+	}
+	if user == nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	response := userGetResponse{
+		Username:    user.Username,
+		DisplayName: user.DisplayName,
+	}
+	c.JSON(http.StatusOK, response)
+
 }
