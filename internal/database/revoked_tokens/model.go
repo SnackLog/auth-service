06fd8dfe5cc94d07b2f0c0e96a53419efb7c1f8f
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2026-01-13 14:08:30 +0100

    Add IsRevoked and move insertion to queries

--- internal/database/revoked_tokens/model.go
+++ internal/database/revoked_tokens/model.go
@@ -1,20 +1,9 @@
 package revokedtokens
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
 )
 
 type RevokedToken struct {
 	TokenID string `db:"token_uuid"`
 }
 
-func RevokeToken(db *sql.DB, tokenID string) (*RevokedToken, error) {
-	_, err := db.Exec("INSERT INTO revoked_tokens (token_uuid) VALUES ($1)", tokenID)
-	if err != nil {
-		log.Println("Failed to revoke token:", err)
-		return nil, fmt.Errorf("Failed to revoke token: %v", err)
-	}
-	return &RevokedToken{TokenID: tokenID}, nil
-}
---
+++ internal/database/revoked_tokens/queries.go
@@ -0,0 +1,26 @@
+package revokedtokens
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+func IsTokenRevoked(db *sql.DB, uuid string) (bool, error) {
+	var exists bool
+	err := db.QueryRow("SELECT EXISTS(SELECT 1 FROM revoked_tokens WHERE token_uuid = ?)", uuid).Scan(&exists)
+	if err != nil {
+		// In case of error, assume the token is revoked
+		return true, err
+	}
+	return exists, nil
+}
+
+func RevokeToken(db *sql.DB, tokenID string) (*RevokedToken, error) {
+	_, err := db.Exec("INSERT INTO revoked_tokens (token_uuid) VALUES ($1)", tokenID)
+	if err != nil {
+		log.Println("Failed to revoke token:", err)
+		return nil, fmt.Errorf("Failed to revoke token: %v", err)
+	}
+	return &RevokedToken{TokenID: tokenID}, nil
+}
